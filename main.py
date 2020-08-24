f55abce3094c9daff02af7b158a46e3a1fcf2564
Alexdieu <alexdieuveult@gmail.com>
2020-08-24 11:48:58 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle
+
+draw = True
+while draw:
+    turtle.pen()
+    turtle.forward(200)
+    turtle.left(90)
+    turtle.forward(1)
+    turtle.left(90)
+    turtle.forward(200)
+    turtle.right(90)
+    turtle.forward(1)
+    turtle.right(90)
+      
